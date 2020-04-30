d1f64d0fa2d87025a01c4573cc9ab0257db2452e
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2020-04-30 22:17:20 +0700

    đau lưng
    huhu

--- blog2/JS/Weather.js
+++ blog2/JS/Weather.js
@@ -1,23 +1,47 @@
 function Weather() {
 
 
+var name = document.getElementById("NameCountry").value;
+$.ajax({
 
-    var name = document.getElementById("NameCountry").value;
-
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=cd90f00b12ce84d2f0a8cd67484eb33c", function (We){
-    var Temp = We.main.temp - 269;
-    var TempMax = We.main.temp_max - 269;
-    var TempMin = We.main.temp_min - 269;
-    var feels =  We.main.feels_like - 269;
-    document.getElementById("temp").innerHTML =parseInt(Temp) + "°C";
-    document.getElementById("Feels").innerHTML =parseInt(feels)+ "°C";
-    document.getElementById("tempX").innerHTML =parseInt(TempMax) + "°C";
-    document.getElementById("tempY").innerHTML =parseInt(TempMin) + "°C";
-    document.getElementById("Am").innerHTML =We.main.humidity + "%";
-    document.getElementById("AVGWinds").innerHTML =We.wind.speed + "m/s";
-    document.getElementById("Clouds").innerHTML =We.clouds.all + "%";
-
-
+    data: 'GET',
+    url: 'http://api.openweathermap.org/data/2.5/weather?q='+name+'&appid=cd90f00b12ce84d2f0a8cd67484eb33c',
+    success: function(We)
+    {
+        var Temp = We.main.temp - 269;
+        var TempMax = We.main.temp_max - 269;
+        var TempMin = We.main.temp_min - 269;
+        var feels =  We.main.feels_like - 269;
+        document.getElementById("temp").innerHTML =parseInt(Temp) + "°C";
+        document.getElementById("Feels").innerHTML =parseInt(feels)+ "°C";
+        document.getElementById("tempX").innerHTML =parseInt(TempMax) + "°C";
+        document.getElementById("tempY").innerHTML =parseInt(TempMin) + "°C";
+        document.getElementById("Am").innerHTML =We.main.humidity + "%";
+        document.getElementById("AVGWinds").innerHTML =We.wind.speed + "m/s";
+        document.getElementById("Clouds").innerHTML =We.clouds.all + "%";
+    },
 
 });
+
 }
+
+
+
+// var name = document.getElementById("NameCountry").value;
+//
+// $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=cd90f00b12ce84d2f0a8cd67484eb33c", function (We){
+//     var Temp = We.main.temp - 269;
+//     var TempMax = We.main.temp_max - 269;
+//     var TempMin = We.main.temp_min - 269;
+//     var feels =  We.main.feels_like - 269;
+//     document.getElementById("temp").innerHTML =parseInt(Temp) + "°C";
+//     document.getElementById("Feels").innerHTML =parseInt(feels)+ "°C";
+//     document.getElementById("tempX").innerHTML =parseInt(TempMax) + "°C";
+//     document.getElementById("tempY").innerHTML =parseInt(TempMin) + "°C";
+//     document.getElementById("Am").innerHTML =We.main.humidity + "%";
+//     document.getElementById("AVGWinds").innerHTML =We.wind.speed + "m/s";
+//     document.getElementById("Clouds").innerHTML =We.clouds.all + "%";
+//
+//
+//
+// });
