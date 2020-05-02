b18a492575a18b00c09687f442ca699d48221e96
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2020-05-02 12:55:14 +0700

    Location + Loading

--- blog2/JS/Weather.js
+++ blog2/JS/Weather.js
@@ -1,5 +1,5 @@
 function Weather() {
-
+    document.getElementById("testLoad").innerHTML = '<img src="Image/loading_spinner.gif">'
 
 var name = document.getElementById("NameCountry").value;
 $.ajax({
@@ -12,36 +12,56 @@ $.ajax({
         var TempMax = We.main.temp_max - 269;
         var TempMin = We.main.temp_min - 269;
         var feels =  We.main.feels_like - 269;
-        document.getElementById("temp").innerHTML =parseInt(Temp) + "°C";
-        document.getElementById("Feels").innerHTML =parseInt(feels)+ "°C";
-        document.getElementById("tempX").innerHTML =parseInt(TempMax) + "°C";
-        document.getElementById("tempY").innerHTML =parseInt(TempMin) + "°C";
-        document.getElementById("Am").innerHTML =We.main.humidity + "%";
-        document.getElementById("AVGWinds").innerHTML =We.wind.speed + "m/s";
-        document.getElementById("Clouds").innerHTML =We.clouds.all + "%";
+        document.getElementById("temp").innerHTML =parseInt(Temp) + " °C";
+        document.getElementById("Feels").innerHTML =parseInt(feels)+ " °C";
+        document.getElementById("tempX").innerHTML =parseInt(TempMax) + " °C";
+        document.getElementById("tempY").innerHTML =parseInt(TempMin) + " °C";
+        document.getElementById("Am").innerHTML =We.main.humidity + " %";
+        document.getElementById("AVGWinds").innerHTML =We.wind.speed + " m/s";
+        document.getElementById("Clouds").innerHTML =We.clouds.all + " %";
+        document.getElementById("LocaName").innerHTML =We.name;
+        document.getElementById("testLoad").innerHTML = "";
     },
 
 });
 
 }
 
+var x = document.getElementById("Locati");
+function getLocation() {
+    document.getElementById("testLoad").innerHTML = '<img src="Image/loading_spinner.gif">'
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation không hỗ trợ trình duyệt của bạn.";
+    }
+
+    function showPosition(position) {
+        var Lat =  position.coords.latitude;
+        var Lon =position.coords.longitude;
+    $.ajax({
+        data: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat='+Lat+'&lon='+Lon+'&appid=cd90f00b12ce84d2f0a8cd67484eb33c',
+        success: function(We)
+        {
+                var Temp = We.main.temp - 269;
+                var TempMax = We.main.temp_max - 269;
+                var TempMin = We.main.temp_min - 269;
+                var feels = We.main.feels_like - 269;
+                document.getElementById("temp").innerHTML = parseInt(Temp) + " °C";
+                document.getElementById("Feels").innerHTML = parseInt(feels) + " °C";
+                document.getElementById("tempX").innerHTML = parseInt(TempMax) + " °C";
+                document.getElementById("tempY").innerHTML = parseInt(TempMin) + " °C";
+                document.getElementById("Am").innerHTML = We.main.humidity + " %";
+                document.getElementById("AVGWinds").innerHTML = We.wind.speed + " m/s";
+                document.getElementById("Clouds").innerHTML = We.clouds.all + " %";
+                document.getElementById("LocaName").innerHTML = We.name;
+                document.getElementById("testLoad").innerHTML = "";
+        },
+    });
+    }
+}
 
 
-// var name = document.getElementById("NameCountry").value;
-//
-// $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=cd90f00b12ce84d2f0a8cd67484eb33c", function (We){
-//     var Temp = We.main.temp - 269;
-//     var TempMax = We.main.temp_max - 269;
-//     var TempMin = We.main.temp_min - 269;
-//     var feels =  We.main.feels_like - 269;
-//     document.getElementById("temp").innerHTML =parseInt(Temp) + "°C";
-//     document.getElementById("Feels").innerHTML =parseInt(feels)+ "°C";
-//     document.getElementById("tempX").innerHTML =parseInt(TempMax) + "°C";
-//     document.getElementById("tempY").innerHTML =parseInt(TempMin) + "°C";
-//     document.getElementById("Am").innerHTML =We.main.humidity + "%";
-//     document.getElementById("AVGWinds").innerHTML =We.wind.speed + "m/s";
-//     document.getElementById("Clouds").innerHTML =We.clouds.all + "%";
-//
-//
-//
-// });
+
+
